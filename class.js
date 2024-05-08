31fe407d646eff1408f7a92fc0ef5f5acdd77a30
Madhubalan-R <madhubalanbe016@gmail.com>
2024-05-08 23:29:35 +0530

    Modified

---
+++ class.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.getElementsByClassName("common");
+    let count = 0;
+
+    for (var i = 0; i < elements.length; i++) {
+        count++;
+    }
+
+    console.log("count is " + count);
+});
+
+    
+
+    
+
+
+
+
+
+
